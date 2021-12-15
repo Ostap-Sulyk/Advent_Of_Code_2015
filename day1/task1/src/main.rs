3ba973944e1456ebe7c8b8955e1fbd79102b9e3f
Ostap-Sulyk <ostapsulyk@gmail.com>
2021-12-15 18:16:40 -0500

    now done

--- day1/task1/src/main.rs
+++ day1/task1/src/main.rs
@@ -3,17 +3,12 @@ fn main() {
     // convert split string and iterate over it to and decrease or increase floor level
     let instructions: Vec<char> = include_str!("input").chars().collect();
     let mut floor = 0;
-    let mut position = 0;
-    for (pos, instruction) in instructions.iter().enumerate() {
+    for instruction in instructions.iter() {
         if *instruction == '(' {
             floor += 1;
         } else if *instruction == ')' {
             floor -= 1;
         }
-        if floor == -1 {
-            position = pos;
-            break;
-        }
     }
-    println!("{}", position);
+    println!("{}", floor);
 }
